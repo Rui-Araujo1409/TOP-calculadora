4e160b8334aab1c1bc219cd38c89e191304ef8ce
Rui-Araujo1409 <rui1409@gmail.com>
2025-09-17 14:33:06 +0000

    fx para inserir nums e operadores ecrã

---
+++ script.js
@@ -0,0 +1,36 @@
+//let operador = ["+", "-", "x", "/"];
+
+const soma = (num1, num2) => num1 + num2;
+const subtracção = (num1, num2) => num1 - num2;
+const multiplicação = (num1, num2) => num1 * num2;
+const divisão = (num1, num2) => num1 / num2;
+
+const operate = (num1, num2, operador) => {
+    switch (operador) {
+        case ("+"):
+            return soma(num1, num2);
+            break;
+        case ("-"):
+            return subtracção(num1, num2);
+            break;
+        case ("x"):
+            return multiplicação(num1, num2);
+            break;
+        case ("/"):
+            return divisão(num1, num2);
+            break;
+    }
+}
+
+
+const ecrã = document.querySelector("#ecrã");
+const numero = document.querySelector(".numero");
+const operador = document.querySelector(".operador");
+
+numero.addEventListener("click", (e) => {
+    ecrã.value += numero.textContent;
+    
+})
+operador.addEventListener("click", (e) => {
+    ecrã.value += operador.textContent;
+})
