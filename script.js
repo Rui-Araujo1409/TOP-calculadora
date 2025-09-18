13666ea3ab9afc198c9ceaeb263f13582023ef74
Rui-Araujo1409 <rui1409@gmail.com>
2025-09-18 15:16:33 +0000

    como guardar os valores?

--- script.js
+++ script.js
@@ -24,8 +24,90 @@ const operate = (num1, num2, operador) => {
 
 
 const ecrã = document.querySelector("#ecrã");
-const numero = document.querySelectorAll(".numero");
-const operador = document.querySelector(".operador");
+const teclado = document.querySelector("#teclado");
+const n0 = document.querySelector("#n0");
+const n1 = document.querySelector("#n1");
+const n2 = document.querySelector("#n2");
+const n3 = document.querySelector("#n3");
+const n4 = document.querySelector("#n4");
+const n5 = document.querySelector("#n5");
+const n6 = document.querySelector("#n6");
+const n7 = document.querySelector("#n7");
+const n8 = document.querySelector("#n8");
+const n9 = document.querySelector("#n9");
+const adição = document.querySelector("#soma");
+const sub = document.querySelector("#sub");
+const multi = document.querySelector("#multi");
+const divi = document.querySelector("#divi");
+const igual = document.querySelector("#igual");
+const apagar = document.querySelector("#apagar");
 
+let valor = [];
+
+teclado.addEventListener("click", (e) => {
+    let target = e.target;
+
+    switch (target.id) {
+        case "n0":
+            ecrã.value += "0";
+            break;
+        case "n1":
+            ecrã.value += "1";
+            break;
+        case "n2":
+            ecrã.value += "2";
+            break;
+        case "n3":
+            ecrã.value += "3";
+            break;
+        case "n4":
+            ecrã.value += "4";
+            break;
+        case "n5":
+            ecrã.value += "5";
+            break;
+        case "n6":
+            ecrã.value += "6";
+            break;
+        case "n7":
+            ecrã.value += "7";
+            break;
+        case "n8":
+            ecrã.value += "8";
+            break;
+        case "n9":
+            ecrã.value += "9";
+            break;
+        case "soma":
+            valor.push(ecrã.value);
+            valor.push("+");
+            console.log(valor);
+            ecrã.value += "+";
+            break;
+        case "sub":
+            valor.push("-");
+            console.log(valor);
+            ecrã.value += "-";
+            break;
+        case "multi":
+            valor.push("x");
+            console.log(valor);
+            ecrã.value += "X";
+            break;
+        case "divi":
+            valor.push("/");
+            console.log(valor);
+            ecrã.value += "/";
+            break;
+        case "igual":
+          valor.push(ecrã.value);
+          console.log(valor); 
+            break;
+        case "apagar":
+            ecrã.value = "";
+            valor = [];
+            break;
+    }
+})
 
 
